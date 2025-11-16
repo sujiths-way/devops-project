32be0156005016b9ab99a644bdfac4e2180c3b65
sujiths-way <sujith.s@way.com>
2025-11-16 19:19:23 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
+
+docker stop react
+docker rm react
+
+
+docker build -t react-ci/cd .
+
+
+docker run -d -it --name react -p 80:80 react-ci/cd
+
+docker tag react-ci/cd sujithway/react-app:ci-cd
+docker push sujithway/react-app:ci-cd
